ddd0dfe94ee1b8ae2777b6534ffc8e2c96739c4c
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-10-16 22:46:25 +0900

    Create beads.py

---
+++ 엘리스/자료구조/배열과연결리스트/구슬넣기(연결리스트)/beads.py
@@ -0,0 +1,104 @@
+'''
+1. LinkedListPipe 클래스를 완성하세요.
+2. procesBeads 함수를 완성하세요.
+'''
+#연결 리스트가 갖고 있는 하나의 정점 
+class LinkedListElement :
+    def __init__(self, val, ptr) :
+        self.value = val #정수 
+        self.myNext = ptr #또다른 element 
+
+class LinkedListPipe:
+    '''
+    Linked List를 이용하여 다음의 method들을 작성하세요.
+    '''
+    def __init__(self) :
+        '''
+        리스트 myPipe를 만듭니다. 이는 구슬의 배치를 저장합니다.
+        '''
+        # 연결 리스트의 시작 정점과 끝 정점을 가리키는 것이 
+        # 각각 start end인데 얘들을 None으로 초기화 
+        #myPipe = []
+
+        self.start = None
+        self.end = None
+        pass 
+
+    def addLeft(self, n) :
+        '''
+        파이프의 왼쪽으로 구슬 n을 삽입합니다.
+        '''
+        if self.start == None and self.end == None : 
+            elem = LinkedListElement(n,None) # 정점 하나 만들어줌 (뒤에 없으니까 포인터에 0  )
+
+            self.start = elem 
+            self.end = elem 
+        else : 
+            elem = LinkedListElement(n,self.start)
+
+            self.start = elem 
+
+                
+
+    def addRight(self, n) :
+        '''
+        파이프의 오른쪽으로 구슬 n을 삽입합니다.
+        '''
+        if self.start == None and self.end == None : 
+            elem = LinkedListElement(n,None)
+
+            self.start = elem 
+            self.end = elem 
+        else : 
+            elem = LinkedListElement(n,None)
+
+            self.end.myNext = elem
+
+            self.end = elem 
+
+    def getBeads(self) :
+        '''
+        파이프의 배치를 list로 반환합니다.
+        '''
+        result = []
+
+
+        current = self.start
+        while current != None : 
+            result.append(current.value)
+            current = current.myNext 
+     
+        return result
+        
+        
+
+
+def processBeads(myInput) :
+    '''
+    구슬을 파이프에 넣는 행위가 myInput으로 주어질 때, 구슬의 최종 배치를 리스트로 반환하는 함수를 작성하세요.
+
+    myInput[i][0] : i번째에 넣는 구슬의 번호
+    myInput[i][1] : i번째에 넣는 방향
+
+    예를 들어, 예제의 경우 
+
+    myInput[0][0] = 1, myInput[0][1] = 0,
+    myInput[1][0] = 2, myInput[1][1] = 1,
+    myInput[2][0] = 3, myInput[2][1] = 0
+
+    입니다.
+
+    '''
+
+    myPipe = LinkedListPipe()
+    
+    for x,y in myInput : 
+        if y == 0 : 
+            myPipe.addLeft(x)
+        elif y == 1 :
+            myPipe.addRight(x)
+
+    result = myPipe.getBeads()
+
+
+    return result
