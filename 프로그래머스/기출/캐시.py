87b57af8c3f219084eda8886d2b2647952e78a84
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2023-02-21 10:09:40 +0900

    deque의 maxlen 활용 방법 추가 https://codetorial.net/tips_and_examples/collections_deque.html

--- 프로그래머스/기출/캐시.py
+++ 프로그래머스/기출/캐시.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(cacheSize, cities):
     time = 0
     cache = []
@@ -17,4 +18,20 @@ def solution(cacheSize, cities):
             
                 
     
+    return time
+
+#방법 2 
+def solution(cacheSize, cities):
+    import collections
+    cache = collections.deque(maxlen=cacheSize)
+    time = 0
+    for i in cities:
+        s = i.lower()
+        if s in cache:
+            cache.remove(s)
+            cache.append(s)
+            time += 1
+        else:
+            cache.append(s)
+            time += 5
     return time
