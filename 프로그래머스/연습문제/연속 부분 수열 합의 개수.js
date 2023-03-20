2ca86f38cf1882421eb699db7f5c933c0e97a32b
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2023-03-20 23:07:03 +0900

    연속 부분 수열 합의 개수 / js 버전 / LV 2
    https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=javascript
    
    슬라이딩 윈도우 :
    https://velog.io/@hanei100/TIL-Sliding-Window

---
+++ 프로그래머스/연습문제/연속 부분 수열 합의 개수.js
@@ -0,0 +1,42 @@
+// 방법 1 (슬라이딩 윈도우)
+const solution = (elements) => {
+    const sumSet = new Set();
+    
+    const len = elements.length;
+    for (let i=1; i<=len; i++) { // 연속 부분 수열의 길이
+        /* 슬라이딩 윈도우 */
+        let sum = 0;
+        for (let j=0; j<len; j++) { // 연속 부분 수열 시작 지점의 인덱스
+            if (j === 0) { // 최초 한 번의 창문에 대해서만 직접 합을 구하기
+                for (let k=0; k<i; k++) {
+                    sum += elements[k];
+                }
+            }
+            else { // 이후 창문들에 대해서는 이전에 구한 합을 활용하기
+                sum -= elements[j-1];
+                sum += elements[(j+i-1) % len];
+            }
+            sumSet.add(sum);
+        }
+        /*****************/
+    }
+    
+    // 원형 수열의 연속 부분 수열 합으로 만들 수 있는 수의 개수를 return
+    return sumSet.size;
+}
+
+// 방법 2 
+function solution(elements) {
+    const set = new Set();
+    for (let i = 1; i <= elements.length; i++) {
+        for (let j = 0; j < elements.length; j++) {
+            let seq = elements.slice(j, j + i);
+            if (i + j > elements.length) {
+                seq = [...seq, ...elements.slice(0, i + j - elements.length)];
+            }
+            const sum = seq.reduce((acc, v) => acc + v, 0);
+            set.add(sum);
+        }
+    }
+    return set.size;
+}
